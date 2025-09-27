6fe6887d42702fff9a4260d8ac8aea877967aca4
taniabarmaksez <taniabarmaksez@gmail.com>
2025-09-27 02:01:29 -0400

    La méthode par dichotomie (recherche binaire)est désormais aussi incluse.

--- TP1.py
+++ TP1.py
@@ -11,7 +11,7 @@ def racine_chiffre_par_chiffre(nombre):
     if nombre < 0:
         raise ValueError("nombre negatif")
     if nombre == 0:
-        return 0
+        return 0.0
     while (partie_entiere + 1) ** 2 <= nombre:
         partie_entiere += 1
     resultat = float(partie_entiere)
@@ -19,8 +19,8 @@ def racine_chiffre_par_chiffre(nombre):
     # le calcul des decimales chiffre par chiffre
     facteur = 10
     for _ in range(partie_decimale):
-        # Essayer chaque chiffre de 0 à 9 pour cette position décimale
-        meilleur_chiffre = 0
+    # Essayer chaque chiffre de 0 à 9 pour cette position décimale
+        meilleur_chiffre = 0.0
         for chiffre in range(10):
             essai = resultat + chiffre / facteur
             if essai ** 2 <= nombre:
@@ -34,3 +34,32 @@ def racine_chiffre_par_chiffre(nombre):
     return resultat
 
 print(racine_chiffre_par_chiffre(8))
+
+# methode 2 dichotomie
+def racine_dichotomie(nombre):
+    precision_decimal = 0.000001
+    if nombre < 0:
+        raise ValueError("nombre negatif")
+    if nombre == 0:
+        return 0.0
+
+    # Déterminer l'intervalle initial
+    if nombre >= 1:  # Si nombre ≥ 1 : intervalle initial = [0, nombre]
+        bas = 0
+        haut = nombre
+    else:  # Si 0 < nombre < 1 : intervalle initial = [nombre, 1]
+        bas = nombre
+        haut = 1
+
+    # Réduction de l'intervalle de moitié
+    while (haut - bas) > precision_decimal:
+        milieu = (bas + haut) / 2
+        if milieu ** 2 < nombre:
+            bas = milieu
+        else:
+            haut = milieu
+
+    resultat = (bas + haut) / 2
+    return resultat
+
+print(racine_dichotomie(8))
